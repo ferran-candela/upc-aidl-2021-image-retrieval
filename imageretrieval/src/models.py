8a929a23b071d2c5077904adf41395b881f53e0b
Ferran Candela <ferran.candela@gmail.com>
2021-04-05 01:48:57 +0200

    Activate all models

--- imageretrieval/src/models.py
+++ imageretrieval/src/models.py
@@ -155,12 +155,12 @@ class Model:
 class ModelManager:
     def __init__(self, device, models_dir):
         self.models =   [  
-                            # 'vgg16', # Documentation says input must be 224x224
-                            # 'resnet50',
-                            # 'inception_v3', # [batch_size, 3, 299, 299]
-                            # 'inception_resnet_v2', #needs : [batch_size, 3, 299, 299]
-                            # 'densenet161',
-                            # 'efficient_net_b4',
+                            'vgg16', # Documentation says input must be 224x224
+                            'resnet50',
+                            'inception_v3', # [batch_size, 3, 299, 299]
+                            'inception_resnet_v2', #needs : [batch_size, 3, 299, 299]
+                            'densenet161',
+                            'efficient_net_b4',
                             'resnet50_custom'
                             ]
                     
